27074d4eccd80557233a2eefd66c5401315834d1
Jeff Miller <jamiller619@gmail.com>
2018-01-24 22:00:39 -0500

    improved structure and module builds

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,40 +1,41 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+import babelMinify from 'rollup-plugin-babel-minify'
 import commonjs from 'rollup-plugin-commonjs'
-import minify from 'rollup-plugin-babel-minify'
-import merge from 'merge-options'
+import cleanup from 'rollup-plugin-cleanup'
 
-const path = {
-  src: (file) => `lib/${ file }`,
-  dest: (file) => `dist/${ file }`
+const fpath = {
+  dev: file => `lib/${ file }`,
+  dist: file => `dist/${ file }`
 }
 
 const config = {}
+const modules = ['', 'router', 'store']
 
-config.default = {
-  input: path.src('index.js'),
-  output: {
-    file: path.dest('jasmin.js'),
-    format: 'umd',
-    name: 'jasmin'
-  },
-  plugins: [
-    babel(),
-    resolve(),
-    commonjs()
-  ]
+const pluginDefs = [resolve(), commonjs(), babel({
+  exclude: 'node_modules/**'
+}), cleanup()]
+
+const make = (module, minify = false) => {
+  const resolvedName = module === '' ? 'jasmin' : module
+  const plugins = minify
+    ? [...pluginDefs, babelMinify({ comments: false })]
+    : pluginDefs
+
+  return {
+    input: fpath.dev(`${ module }/index.js`),
+    output: {
+      file: `${ fpath.dist(resolvedName) }${ minify ? '.min' : '' }.js`,
+      format: 'cjs'
+      // format: 'iife',
+      // name: resolvedName
+    },
+    plugins: plugins,
+    interop: false
+  }
 }
 
-config.minify = merge(config.default, {
-  output: {
-    file: path.dest('jasmin.min.js')
-  },
-  plugins: [
-    babel(),
-    resolve(),
-    commonjs(),
-    minify()
-  ]
-})
+config.default = modules.map(module => make(module))
+config.minify = modules.map(module => make(module, true))
 
 export default config[process.env.config]
