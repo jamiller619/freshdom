08435ad7c6d371fceabd8a4c06243f70a0846045
Jeff Miller <jamiller619@gmail.com>
2018-02-01 15:15:29 -0500

    updated license and added banner to bundles

---
+++ build/banner.js
@@ -0,0 +1,13 @@
+import pkg from '../package.json'
+
+export default `
+/**
+ * jasmin v${ pkg.version }
+ *
+ * Copyright (c) 2017-2018, Jeff Miller, jamiller.me
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license, which can be found in the LICENSE file located in this projects root directory.
+ *
+ */
+`
